a58eb3fa462a49c74b89f781bc27e09d14822ed8
rain474 <lordraindance5@gmail.com>
2026-02-08 18:38:23 -0800

    Rewrote nvim-treesitter installation as the previous code didn't really work
    https://github.com/nvim-treesitter/nvim-treesitter/issues/8424#issuecomment-3744851561
    thanks to poe

--- init.lua
+++ init.lua
@@ -13,9 +13,12 @@ vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
-vim.o.autoindent = true
 
 vim.o.winborder = "rounded"
+
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.cindent = true
 
 -- OPTIONS>diagnostic
 vim.diagnostic.config({
@@ -68,13 +71,6 @@ vim.keymap.set("n", "<leader>e", function()
 end, { desc = "File Explorer" })
 
 -- treesitter-poryscript
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "pory",
-  callback = function()
-    vim.treesitter.start()
-  end,
-})
-
 vim.api.nvim_create_autocmd('User', {
   pattern = 'TSUpdate',
   callback = function()
@@ -89,17 +85,51 @@ vim.api.nvim_create_autocmd('User', {
 })
 vim.treesitter.language.register('poryscript', { 'pory' })
 
-require("nvim-treesitter.config").setup({
-    ensure_installed = { "lua", "c", "vimdoc", "markdown", "vim", "json", "python", "poryscript" },
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
+require('nvim-treesitter').install({
+  "lua", "c", "vimdoc", "markdown", "vim", "json", "python", "poryscript", "zig"
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "lua", "c", "vimdoc", "markdown", "vim", "json", "python", "poryscript", "zig", "cpp", "cmake",
+  },
+  callback = function()
+    vim.treesitter.start()
+
+    vim.o.autoindent = false
+    vim.o.smartindent = false
+    vim.o.cindent = false
+    vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+  end,
+})
+
+-- If you want to try indentation with nvim-treesitter. In my experience, it's pretty awful
+-- vim.keymap.set('n', '<leader>sT', function()
+--   local current_indentexpr = vim.bo.indentexpr
+--   local current_buffer = vim.api.nvim_get_current_buf()
+--   if current_indentexpr ~= "v:lua.require('nvim-treesitter').indentexpr()" then
+--     vim.b[current_buffer].rahlir_previous_indentexpr = current_indentexpr
+--     vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+--     vim.print("Turned on treesitter indent...")
+--     vim.o.autoindent = false
+--     vim.o.smartindent = false
+--     vim.o.cindent = false
+--   else
+--     local previous_indentexpr = vim.b[current_buffer].rahlir_previous_indentexpr
+--     if previous_indentexpr == nil then
+--       previous_indentexpr = ""
+--     end
+--     vim.bo.indentexpr = previous_indentexpr
+--     vim.o.autoindent = true
+--     vim.o.smartindent = true
+--     vim.o.cindent = true
+--     vim.print("Turned off treesitter indent...")
+--   end
+-- end, { noremap=true, silent=true, desc="Toggle treesitter indent" })
 
 -- LSPCONFIG
 require("blink-cmp").setup({
   keymap = { preset = "enter" },
 })
 
-vim.lsp.enable({"lua-ls", "poryscript", "clangd"})
+vim.lsp.enable({"lua-ls", "poryscript", "clangd", "zls"})
